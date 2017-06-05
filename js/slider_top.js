1f6bf15034e2f2ea1f685c233e11c4a31f963462
botjoker <botjoker@gmail.com>
2017-06-06 00:09:16 +0300

    test

--- js/slider_top.js
+++ js/slider_top.js
@@ -1,14 +1,12 @@
-var mySwiper = new Swiper('.swiper-container',{
-    pagination: '.pagination',
-    loop:true,
-    grabCursor: true,
-    paginationClickable: true
-  })
-  $('.arrow-left').on('click', function(e){
-    e.preventDefault()
-    mySwiper.swipePrev()
-  })
-  $('.arrow-right').on('click', function(e){
-    e.preventDefault()
-    mySwiper.swipeNext()
-  })
+
+$('#top-slider').slick({
+    nextArrow: '<a class="arrow-right" href="#"></a>',
+    prevArrow: '<a class="arrow-left" href="#"></a>',
+});
+
+$('#items-slider1').slick({
+    prevArrow: '<div style="color: red;">prev</div>',
+    nextArrow: '<div style="color: red;">nxt</div>',
+});
+
+
