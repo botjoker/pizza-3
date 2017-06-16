c118202c50b3ced20c54aa7310e76ac830d5d5d2
botjoker <botjoker@gmail.com>
2017-06-16 08:14:45 +0300

    test

--- js/slider_top.js
+++ js/slider_top.js
@@ -5,21 +5,21 @@ $('#top-slider').slick({
 });
 
 $('#items-slider1').slick({
-    prevArrow: '<span href="#block-1" class="gallery__link-left"><img src="img/arrow_gallery.png" ></span>',
-    nextArrow: '<span href="#block-1" class="gallery__link-right"><img src="img/arrow_gallery.png" ></span>',
+    prevArrow: '<span href="#block-1" class="gallery__link-left"><img src="img/arrow_gallery_left.jpg" ></span>',
+    nextArrow: '<span href="#block-1" class="gallery__link-right"><img src="img/arrow_gallery_right.jpg" ></span>',
 });
 
 $('#items-slider2').slick({
     slidesToShow: 4,
     slidesToScroll: 4,
-    prevArrow: '<span href="#block-1" class="gallery__link-left"><img src="img/arrow_gallery.png" ></span>',
-    nextArrow: '<span href="#block-1" class="gallery__link-right"><img src="img/arrow_gallery.png" ></span>',
+    prevArrow: '<span href="#block-1" class="gallery__link-left"><img src="img/arrow_gallery_left.jpg" ></span>',
+    nextArrow: '<span href="#block-1" class="gallery__link-right"><img src="img/arrow_gallery_right.jpg" ></span>',
 });
 
 
 $('#items-slider3').slick({
     slidesToShow: 4,
     slidesToScroll: 4,
-    prevArrow: '<span href="#block-1" class="gallery__link-left"><img src="img/arrow_gallery.png" ></span>',
-    nextArrow: '<span href="#block-1" class="gallery__link-right"><img src="img/arrow_gallery.png" ></span>',
+    prevArrow: '<span href="#block-1" class="gallery__link-left"><img src="img/arrow_gallery_left.jpg" ></span>',
+    nextArrow: '<span href="#block-1" class="gallery__link-right"><img src="img/arrow_gallery_right.jpg" ></span>',
 });
