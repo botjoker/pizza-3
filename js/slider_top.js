f2dc1b0c013f3ae36cc1c51e50bd21744a6a145b
botjoker <botjoker@gmail.com>
2017-06-16 08:11:11 +0300

    test

--- js/slider_top.js
+++ js/slider_top.js
@@ -1,12 +1,25 @@
 
 $('#top-slider').slick({
-    nextArrow: '<a class="arrow-right" href="#"></a>',
-    prevArrow: '<a class="arrow-left" href="#"></a>',
+    nextArrow: '<span href="#block-1" class="gallery__link-right"><img src="img/arrow_gallery.png" ></span>',
+    prevArrow: '<span href="#block-1" class="gallery__link-left"><img src="img/arrow_gallery.png" ></span>',
 });
 
 $('#items-slider1').slick({
-    prevArrow: '<div style="color: red;">prev</div>',
-    nextArrow: '<div style="color: red;">nxt</div>',
+    prevArrow: '<span href="#block-1" class="gallery__link-left"><img src="img/arrow_gallery.png" ></span>',
+    nextArrow: '<span href="#block-1" class="gallery__link-right"><img src="img/arrow_gallery.png" ></span>',
+});
+
+$('#items-slider2').slick({
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    prevArrow: '<span href="#block-1" class="gallery__link-left"><img src="img/arrow_gallery.png" ></span>',
+    nextArrow: '<span href="#block-1" class="gallery__link-right"><img src="img/arrow_gallery.png" ></span>',
 });
 
 
+$('#items-slider3').slick({
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    prevArrow: '<span href="#block-1" class="gallery__link-left"><img src="img/arrow_gallery.png" ></span>',
+    nextArrow: '<span href="#block-1" class="gallery__link-right"><img src="img/arrow_gallery.png" ></span>',
+});
