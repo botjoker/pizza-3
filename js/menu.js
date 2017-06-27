e181de715dacfc283c2dbba902bbacfa4b532f09
botjoker <botjoker@gmail.com>
2017-06-27 09:07:23 +0300

    test

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,15 @@
 "use strict";
 
 $(document).ready(function(){
+
+	var touch = $('#touch-menu');
+	var menu = $('.menu__top-list');
+
+	$(touch).on('click', function(e) {
+		e.preventDefault();
+		menu.slideToggle();
+	});
+
 	$(window).resize(function(){
 		var wid = $(window).width();
 		if(wid > 760 && menu.is(':hidden')) {
--- js/slider_top.js
+++ js/slider_top.js
@@ -1,25 +1,52 @@
 
 $('#top-slider').slick({
-    nextArrow: '<span href="#block-1" class="gallery__link-right"><img src="img/arrow_gallery.png" ></span>',
-    prevArrow: '<span href="#block-1" class="gallery__link-left"><img src="img/arrow_gallery.png" ></span>',
+    nextArrow: '<span class="gallery__link-right"><img src="img/arrow_gallery.png" ></span>',
+    prevArrow: '<span class="gallery__link-left"><img src="img/arrow_gallery.png" ></span>',
 });
 
 $('#items-slider1').slick({
-    prevArrow: '<span href="#block-1" class="gallery__link-left"><img src="img/arrow_gallery_left.jpg" ></span>',
-    nextArrow: '<span href="#block-1" class="gallery__link-right"><img src="img/arrow_gallery_right.jpg" ></span>',
+    prevArrow: '<span class="link-left">s</span>',
+    nextArrow: '<span class="link-right">s</span>',
 });
+$('.gallery1__link-right').click(function(){
+    $('#items-slider1').slick("slickNext");
+});
+$('.gallery1__link-left').click(function(){
+    $('#items-slider1').slick("slickPrev");
+});
+
 
 $('#items-slider2').slick({
     slidesToShow: 4,
     slidesToScroll: 4,
-    prevArrow: '<span href="#block-1" class="gallery__link-left"><img src="img/arrow_gallery_left.jpg" ></span>',
-    nextArrow: '<span href="#block-1" class="gallery__link-right"><img src="img/arrow_gallery_right.jpg" ></span>',
+    prevArrow: '<span class="link-left">s</span>',
+    nextArrow: '<span class="link-right">s</span>',
+
+    customPaging: function (slider, i) {
+        console.log(i);
+        return  (i + 1) + '/' + slider.slideCount;
+    },
+
 });
+$('.gallery2__link-right').click(function(){
+    $('#items-slider2').slick("slickNext");
+});
+$('.gallery2__link-left').click(function(){
+    $('#items-slider2').slick("slickPrev");
+});
+
 
 
 $('#items-slider3').slick({
     slidesToShow: 4,
     slidesToScroll: 4,
-    prevArrow: '<span href="#block-1" class="gallery__link-left"><img src="img/arrow_gallery_left.jpg" ></span>',
-    nextArrow: '<span href="#block-1" class="gallery__link-right"><img src="img/arrow_gallery_right.jpg" ></span>',
+    prevArrow: '<span class="link-left">s</span>',
+    nextArrow: '<span class="link-right">s</span>',
+});
+
+$('.gallery3__link-right').click(function(){
+    $('#items-slider3').slick("slickNext");
+});
+$('.gallery3__link-left').click(function(){
+    $('#items-slider3').slick("slickPrev");
 });
